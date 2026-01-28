75851353cedbdc50d0236f36e57f27abfe12d187
hanari <irfanmaulanahakim04@gmail.com>
2026-01-28 12:13:44 +0700

    Add conditional statements examples in Python
    pengulangan dengan satu, dua aksi atau lebih

---
+++ blok-pengulangan.py
@@ -0,0 +1,44 @@
+if kondisi:
+  aksi1      #harus diawali karakter spasi atau tab
+  aksi2      #harus diawali karakter spasi atau tab
+
+#Jika hanya terdiri dari satu aksi
+if kondisi:aksi
+
+x = 10
+if x % 2 = 0:
+  print ("%d merupakan bilangan genap")
+
+
+#Jika terdiri dari dua aksi
+if kondisi:
+  aksi1
+else:
+  aksi2
+
+x = -17
+if x > 0:
+  print("%d merupakan bilangan positif" % x)
+else:
+  print("%d bukan bilangan positif" % x)
+
+
+#Jika terdiri dari tiga aksi atau lebih
+if kondisi1:
+  aksi1
+elif kondisi2:
+  aksi2
+elif kondisi3:
+  aksi3
+else:
+  aksi4
+
+x = 0
+if x > 0:
+  print("%d bilangan positif" %x x)
+elif x == 0:
+  print("anda memasukan nilai 0")
+else:
+  print("%d bilangan negatif" % x)
+
+
