d2d835879355cae7f4f97f945d9e81ce41f4c56d
hanari <irfanmaulanahakim04@gmail.com>
2026-03-10 16:22:43 +0700

    Add files via upload

---
+++ loopingvertical.py
@@ -0,0 +1,17 @@
+for i in range(3):      #loop ulangi sebanyak 3 kali
+    print("x")
+print()
+
+x = "*"
+for i in range(5):      #looping dimulai dari indeks 0 berhenti di 4
+    print(i)
+print()
+
+x = "*"
+for i in range(1, 6):   #looping dimulai dari indeks 1 (loop 5 kali) berhenti di 5  
+    print(i)
+print()
+
+x = "*"
+for i in range(1, 6):
+    print(x * i)        #kalikan dengan i tiap indeks
