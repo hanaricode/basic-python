f74276ec1cbc68ff868eb1b8498c870beded123d
hanari <irfanmaulanahakim04@gmail.com>
2026-03-16 13:13:27 +0700

    Add division and addition functionality in division.py

---
+++ division.py
@@ -0,0 +1,15 @@
+# division
+num3 = float(input("Masuka angka yang ingin dibagi: "))
+num4 = float(input("Masuka angka pembagi: "))
+if num4 == 0:
+  print("error: tidak bisa dibagi dengan nol")
+else:
+  div_result = num3 / num4
+  print(f"divison: {num3} / {num4} = {div_result}")
+
+
+# addition
+num1 = float(input("masukan angka: "))
+num2 = float(input("masukan angka: "))
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result}")
