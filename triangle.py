b3edc79b6f98c1938a2110818d5f011106c644be
hanaricode <irfanmaulanahakim04@gmail.com>
2026-03-22 09:17:41 +0700

    remove file, move to project-python

--- triangle.py
+++
@@ -1,7 +0,0 @@
-# Progam menghitung luas segitiga
-
-base = float(input("masukan nilai lebar untuk segitiga: "))
-height = float(input("masukan nilai tinggi untuk segitiga: "))
-# mengihitung luas segitiga
-area = 0.5 * base * height
-print(f"Luas segitiga adalah: {area}")
